0b3f21a9f40c14b087bad44b2b0cd5490ed8363b
Siim <siimsuu@hotmail.com>
2018-09-11 16:33:52 +0300

    created empty spring-boot project

---
+++ src/main/java/ee/ria/sessionservice/SessionServiceApplication.java
@@ -0,0 +1,11 @@
+package ee.ria.sessionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SessionServiceApplication.class, args);
+	}
+}
